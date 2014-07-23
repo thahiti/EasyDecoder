93c5be83fc6ec4e2df2956ad5ecd8c8c41451419
thahiti <thahiti@gmail.com>
2014-07-23 15:27:06 +0900

    ffmpeg, x264 files are imported in the project.

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,45 +1,43 @@
 LOCAL_PATH := $(call my-dir)
-FFMPEG := ../../ffmpeg
-X264 := ../../x264
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := libavcodec
-LOCAL_SRC_FILES         := $(FFMPEG)/output/libavcodec-55.so
+LOCAL_SRC_FILES         := ffmpeg/libavcodec/libavcodec-55.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := libavfilter
-LOCAL_SRC_FILES         := $(FFMPEG)/output/libavfilter-4.so
+LOCAL_SRC_FILES         := ffmpeg/libavfilter/libavfilter-4.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := libavformat
-LOCAL_SRC_FILES         := $(FFMPEG)/output/libavformat-55.so
+LOCAL_SRC_FILES         := ffmpeg/libavformat/libavformat-55.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := libavutil
-LOCAL_SRC_FILES         := $(FFMPEG)/output/libavutil-52.so
+LOCAL_SRC_FILES         := ffmpeg/libavutil/libavutil-52.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := libpostproc
-LOCAL_SRC_FILES         := $(FFMPEG)/output/libpostproc-52.so
+LOCAL_SRC_FILES         := ffmpeg/libpostproc/libpostproc-52.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := libswresample
-LOCAL_SRC_FILES         := $(FFMPEG)/output/libswresample-0.so
+LOCAL_SRC_FILES         := ffmpeg/libswresample/libswresample-0.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := libswscale
-LOCAL_SRC_FILES         := $(FFMPEG)/output/libswscale-2.so
+LOCAL_SRC_FILES         := ffmpeg/libswscale/libswscale-2.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := libx264
-LOCAL_SRC_FILES         := $(X264)/output/libx264.so
+LOCAL_SRC_FILES         := x264/libx264.so
 include $(PREBUILT_SHARED_LIBRARY)
 	
 include $(CLEAR_VARS)
@@ -48,7 +46,7 @@ LOCAL_SRC_FILES += NativeVideoDecoder.cpp\
 				   EasyAVCDecoder.cpp\
 				   FScaler.cpp
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(FFMPEG)
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/ffmpeg
 
 LOCAL_SHARED_LIBRARIES := libavcodec\
 						  libavformat\
